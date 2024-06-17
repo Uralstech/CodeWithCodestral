b104235eaaee0d8f32a4776c61ebbfc7aa19e16a
Udayshankar Ravikumar <uralstech@gmail.com>
2024-06-17 09:22:28 +0530

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace CodeWithCodestral
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            CounterBtn.Text = count == 1
+                ? $"Clicked {count} time"
+                : $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CodeWithCodestral
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
